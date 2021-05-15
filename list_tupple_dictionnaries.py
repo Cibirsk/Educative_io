aa194e24b5e9ffdd0ab9391fb94d46b7e71eb7bf
Cibirsk <stvl@gmx.com>
2021-05-15 17:55:08 +0200

    none

---
+++ list_tupple_dictionnaries.py
@@ -0,0 +1,6 @@
+my_list=['b','a','2']
+my_list.sort()
+a=my_list
+print(a)
+
+myDic={'one':}
--- string.py
+++ string.py
@@ -1,4 +1,9 @@
-a='123'
-b=123
-c=' az ert y   '
-print(type(c))
+print("%(lang)s is fun!" % {"lang":"Python"})
+
+print("%(x)i + %(y)i = %(z)i" % {"x":1, "y":2, "z":3})
+
+language={"lang1":"Python","lang2":"Lisp"}
+
+print("%(lang1)s is simpliest than %(lang2)s"  %{"lang1":"Python","lang2":"Lisp"})
+
+print("{lang2} is harder than {lang1}".format(**language))
