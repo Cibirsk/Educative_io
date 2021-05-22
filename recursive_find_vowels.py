17bf3d5458f43ad0a9315629ad1dc4abc6e8328b
Cibirsk <stvl@gmx.com>
2021-05-22 17:42:43 +0200

    Update recursive_find_vowels.py

--- recursive_find_vowels.py
+++ recursive_find_vowels.py
@@ -1,4 +1,21 @@
 #iterative version
-
 def vowelsIter(string):
-    listVowels=['a','e','i','o','u']
+    listVowels='aeiou'
+    numbVowels=0
+    for i in string:
+        for j in listVowels:
+            if i == j:
+                numbVowels +=1
+    return numbVowels
+
+print(vowelsIter('educative'))
+
+#recursive version
+def vowelsRecu(string,n,list):
+    if n==1 and 
+
+
+string='educative'
+n=len(string)
+listVowels='aeiou'
+print(vowelsRecu(string,n,listVowels))
