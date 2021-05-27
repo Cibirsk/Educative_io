b9be981ec74b33b15bcfc9760d865941e38ed426
Cibirsk <stvl@gmx.com>
2021-05-27 09:26:53 +0200

    Update recursive_first_number.py

--- recursive_first_number.py
+++ recursive_first_number.py
@@ -2,5 +2,17 @@ arr = [9, 8, 1, 8, 1, 7]
 targetNumber = 1
 currentIndex = 0
 
+"""
 def findNumb(arr,targetNumber,currentIndex):
-    
+    if targetNumber not in arr[currentIndex]:
+        return -1
+"""    
+
+def findNumbIte(arr,targetNumber,currentIndex):
+    for i in arr:
+        if arr[i]== targetNumber:
+            return i
+        else:
+            return -1
+
+print(findNumbIte(arr,targetNumber,currentIndex))
